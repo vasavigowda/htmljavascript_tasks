35b6253e174fadbea30bc8ef2a9ae589923e2ca9
vasavi <vbalashankarostb2@gmail.com>
2020-02-19 22:38:46 +0530

    first commit

---
+++ task1/js/task1.js
@@ -0,0 +1,13 @@
+var a = "Hello";
+var b = "Welcome";
+var c = "Javascript";
+var d = "Hii good afternoon Had your lunch";
+document.write(a + "<br>" + b + "<br>" + c + "<br>" + d)
+document.getElementById("demo").innerHTML =
+a + "<br>" + b + "<br>" + c + "<br>" + d;
+
+
+
+
+
+ 
---
+++ task3/task.js
@@ -0,0 +1,21 @@
+// function  goodMorning()
+// {
+// 	document.write("goodmorning")
+// }
+// function  goodafternoon()
+// {
+// 	document.write("goodmorning")
+// }
+// function  goodevening()
+// {
+// 	document.write("goodmorning")
+// }
+// var myDate = new Date();
+//     var hrs = myDate.getHours();
+//     if (hrs < 12)
+//        goodMorning();
+//     else if (hrs >= 12 && hrs <= 15)
+//         goodafternoon();
+//     else if (hrs >= 15 && hrs <= 24)
+//         goodevening();
+//    
